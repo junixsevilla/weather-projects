0cc93fbd39debf318e6bdcc1cb8e95cb237fd9b5
Junix <jaranco@gmail.com>
2021-03-12 12:38:59 +1300

    add updated file

--- app.js
+++ app.js
@@ -1,51 +1,57 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const https = require ("https");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Christchurch&appid=bef59ca4cda1ef9fe53966342b5bc9c8&units=metric"
-    
-    https.get(url, function(response) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    const query = req.body.cityName;
+        const unit = "metric";
+        const apiKey = "bef59ca4cda1ef9fe53966342b5bc9c8";
+        const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+        
+  
+
+   https.get(url, function(response) {
         console.log("Status code is: " + response.statusCode);
 
-        response.on("data", function(data) {
+        response.on("data", function(data) {           
+
             const weatherData = JSON.parse(data);
             const description = weatherData.weather[0].description; 
             const temp = weatherData.main.temp
             const weatherIcon = weatherData.weather[0].icon;
             const imageUrl = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
 
+ 
+            res.write("<h1> Weather App </h1>");
 
-            console.log("Weather description : " + description);
-            console.log("Temp is: " + temp);
+            res.write("<p> The temperature in <b>" + query +  "</b> is  <b>" + temp + "</b> degree Celcius </p>");
 
-            res.write("<p> The temperature in Christchurch is " + temp + " degree Celcius </p>");
-
-            res.write("<p> The weather condistion is " + description + "</p>");
+            res.write("<p> The weather condistion is <b>" + description + "</b> </p>");
 
             res.write("<img src =" + imageUrl +">");
 
+            res.write('<br> <a href="/"> Reset </a>');
+
             res.send();
 
-            // reverse of parse = stringify
-            //const object =  {
-            //    name: "Junix",
-            //    age: 30,
-            //}
-            //console.log(JSON.stringify(object));
+
         });
         
 
     } );
-
-  // res.send("Server is up and running!");
 });
-
 
 
 app.listen(3000, function() {
     console.log("server is running on port 3000");
 });
 
-// api key: bef59ca4cda1ef9fe53966342b5bc9c8
+    
